f5ba9a6787b024b85a9c037722612f9d9a901cfc
Nhoxboon <trannhatanh156@gmail.com>
2025-06-24 23:57:21 +0700

    Add tile system and editor for easier to create game level

---
+++ Assets/_Data/TileSystem/TileSetHolder.cs
@@ -0,0 +1,9 @@
+﻿
+using UnityEngine;
+
+public class TileSetHolder : MonoBehaviour
+{
+    public GameObject tileRoad;
+    public GameObject tileField;
+    public GameObject tileSideway;
+}
---
+++ Assets/_Data/TileSystem/TileSlot.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TileSlot : MonoBehaviour
+{
+    //This jyst work in the editor so not causing performance issues
+    protected MeshRenderer MeshRenderer => GetComponent<MeshRenderer>();
+    protected MeshFilter MeshFilter => GetComponent<MeshFilter>();
+    
+    public void SwitchTile(GameObject referanceTile)
+    {
+        TileSlot newTile = referanceTile.GetComponent<TileSlot>();
+
+        MeshFilter.mesh = newTile.GetMesh();
+        MeshRenderer.material = newTile.GetMaterial();
+
+        foreach (var obj in GetAllChildren()) DestroyImmediate(obj);
+        
+        foreach (var obj in newTile.GetAllChildren()) Instantiate(obj, transform);
+        
+    }
+    
+    public Material GetMaterial() => MeshRenderer.sharedMaterial;
+    public Mesh GetMesh() => MeshFilter.sharedMesh;
+    
+    public List<GameObject> GetAllChildren()
+    {
+        return (from Transform child in transform select child.gameObject).ToList();
+        
+        // Same as above but using a for loop
+        // List<GameObject> children = new List<GameObject>();
+        // foreach (Transform child in transform)
+        // {
+        //     children.Add(child.gameObject);
+        // }
+        // return children;
+    }
+}
